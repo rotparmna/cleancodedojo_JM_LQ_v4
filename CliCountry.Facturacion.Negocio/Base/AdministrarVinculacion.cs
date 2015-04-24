21c953fe8c70cc2108af395af96703aa3d74b394
Michael Navarro Ariza <rotpar@gmail.com>
2015-04-24 17:55:03 -0500

    Se sube código de Jose y Luis Fernando
    Se sube el código del 4to Dojo de Nombramiento

---
+++ CliCountry.Facturacion.Negocio/Base/AdministrarVinculacion.cs
@@ -0,0 +1,33 @@
+﻿using CliCountry.SAHI.Dominio.Entidades.Facturacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliCountry.Facturacion.Negocio.Base
+{
+    public class AdministrarVinculacion
+    {
+        /// <summary>
+        /// Busca información de una Vinculación a partir del identificador de la entidad y su número de orden.
+        /// </summary>
+        /// <param name="identificadorEntidad">Identificador de la entidad.</param>
+        /// <param name="ordenVinculacion">Número de orden.</param>
+        /// <returns>
+        /// Retorna la Vinculacion.
+        /// </returns>
+        public static Vinculacion BuscarVinculacion(int identificadorEntidad, int ordenVinculacion, List<Vinculacion> vinculaciones)
+        {
+            var vinculacion = from
+                                  item in vinculaciones
+                              where
+                                  item.Tercero.Id == identificadorEntidad
+                                  && item.Orden == ordenVinculacion
+                              select
+                                  item;
+
+            return vinculacion.FirstOrDefault();
+        }
+    }
+}
